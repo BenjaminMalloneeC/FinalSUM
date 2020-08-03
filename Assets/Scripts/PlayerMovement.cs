e4f8a05a722ef96b2bd7f300c5fde4c1cc7cf841
Ben Mallonee <ben.mallonee.c@gmai.com>
2020-08-03 12:06:04 -0700

    Movement Added
    https://www.kenney.nl/assets/topdown-shooter

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Transform tf;
+    public float turnSpeed = 175f;
+    public float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.instance.player = this.gameObject;
+        tf = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            tf.Rotate(0, 0, turnSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            tf.Rotate(0, 0, -turnSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            tf.position += tf.right * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tf.position += -tf.right * moveSpeed * Time.deltaTime;
+        }
+    }
+}
