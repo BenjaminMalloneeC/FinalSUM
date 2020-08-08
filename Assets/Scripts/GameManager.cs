9a745243c1db5683f7eaba5ee29b2526d849bfef
Ben Mallonee <ben.mallonee.c@gmai.com>
2020-08-07 19:30:50 -0700

    Added Camera Movement
    https://www.youtube.com/watch?v=U66VYM-ShVg&t=1s

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,10 +6,15 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public GameObject player;
+
+    //Camera follow player code
+    public CameraFollow cameraFollow;
+    public Transform playerTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Camera follows player positon
+        cameraFollow.Setup(() => playerTransform.position);
     }
 
     // Update is called once per frame
